bd5fc871d463f5cae6dc07113c46a10372fe233f
Abdallah Eslah <70037909+AbdullahEslah@users.noreply.github.com>
2025-07-17 17:24:21 +0300

    Update universal.dart

--- lib/src/flutter/universal.dart
+++ lib/src/flutter/universal.dart
@@ -732,7 +732,8 @@ class VxUniversal extends StatelessWidget {
 
   Widget cardWidget(Widget current, BuildContext context) {
     final ThemeData theme = Theme.of(context);
-    final CardTheme cardTheme = CardTheme.of(context);
+    //final CardTheme cardTheme = CardTheme.of(context);
+    final cardTheme = Theme.of(context).cardTheme;
     return material(current,
         mType: MaterialType.card,
         mShadowColor: shadowColor ?? cardTheme.shadowColor ?? theme.shadowColor,
